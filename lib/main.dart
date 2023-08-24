647fc877927dbbdba66a3db4e9c63a9a9df0ec50
Leticia <124852398+LeticiaBHB@users.noreply.github.com>
2023-08-23 23:11:18 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,14 @@ void main() {
   ));
 }
 
+class IMCInfo {
+  double weight;
+  double height;
+  DateTime dateTime;
+
+  IMCInfo(this.weight, this.height, this.dateTime);
+}
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -16,8 +24,8 @@ class _HomeState extends State<Home> {
   TextEditingController heightController = TextEditingController();
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
   String _infoText = "Informe seus dados!";
+  List<IMCInfo> imcRecords = [];
 
   void _resetFields() {
     weightController.text = "";
@@ -33,101 +41,155 @@ class _HomeState extends State<Home> {
       double weight = double.parse(weightController.text);
       double height = double.parse(heightController.text) / 100;
       double imc = weight / (height * height);
+
+      IMCInfo info = IMCInfo(weight, height, DateTime.now());
+      imcRecords.add(info);
+
       if (imc < 18.0) {
         _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 18.6 && imc <24.9){
+      } else if (imc >= 18.6 && imc < 24.9) {
         _infoText = "Peso Ideal(${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 24.9 && imc < 29.9){
+      } else if (imc >= 24.9 && imc < 29.9) {
         _infoText = "Levemente Acima do Peso(${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 29.9 && imc < 34.9){
+      } else if (imc >= 29.9 && imc < 34.9) {
         _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 34.9 && imc < 39.9){
+      } else if (imc >= 34.9 && imc < 39.9) {
         _infoText = "Obesidade Grau II(${imc.toStringAsPrecision(4)})";
-      } else if (imc >= 40){
+      } else if (imc >= 40) {
         _infoText = "Obesidade Grau III(${imc.toStringAsPrecision(4)})";
       }
     });
   }
 
+  void _showRecords() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => IMCRecordsScreen(imcRecords)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Calculadora de IMC"),
-          centerTitle: true,
-          backgroundColor: Colors.green,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: _resetFields,
-            )
-          ],
-        ),
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Icon(
-                  Icons.person_search_rounded,
-                  size: 120.0,
-                  color: Colors.lightGreenAccent,
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "Peso (kg)",
-                      labelStyle: TextStyle(color: Colors.green)),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.green, fontSize: 25.0),
-                  controller: weightController,
-                  validator: (value){
-                    if (value!.isEmpty){
-                      return "Insira seu Peso";
-                    }
-                  },
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "Altura(cm)",
-                      labelStyle: TextStyle(color: Colors.green)),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.green, fontSize: 25.0),
-                  controller: heightController,
-                  validator: (value){
-                    if (value!.isEmpty){
-                      return "Insira sua Altura";
-                    }
-                  },
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Container(
-                    height: 50.0,
-                    child: ElevatedButton(
-                      onPressed: (){
-                        if(_formKey.currentState!.validate()){
-                          _calculate();
-                          }
-                        },
-                        child: Text(
-                          "Calcular",
-                           style: TextStyle(color: Colors.white, fontSize: 25.0),
-                        ),
-                      ),
+      appBar: AppBar(
+        title: Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: _resetFields,
+          ),
+          IconButton(
+            icon: Icon(Icons.bookmark_border_sharp),
+            onPressed: _showRecords,
+          ),
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.person_search_rounded,
+                size: 120.0,
+                color: Colors.lightGreenAccent,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: weightController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Insira seu Peso";
+                  }
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura(cm)",
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: heightController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Insira sua Altura";
+                  }
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _calculate();
+                      }
+                    },
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
                     ),
                   ),
-                Text(_infoText,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.green, fontSize: 25.0))
-              ],
-            ),
-            ),
+                ),
+              ),
+              Text(_infoText,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 20.0)),
+              SizedBox(height: 100),
+              Row(
+                children: [
+                  Container(
+                    child: Text('Aqui está seu arquivo de IMC anteriores', style: TextStyle(color: Colors.green, fontSize: 20.0)),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.bookmark_border_sharp),
+                    onPressed: _showRecords,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
+      ),
     );
   }
 }
+
+class IMCRecordsScreen extends StatelessWidget {
+  final List<IMCInfo> imcRecords;
+
+  IMCRecordsScreen(this.imcRecords);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("Registros de IMC"),
+      ),
+      body: ListView.builder(
+        itemCount: imcRecords.length,
+        itemBuilder: (context, index) {
+          IMCInfo record = imcRecords[index];
+          return ListTile(
+            title: Text("Data e Hora: ${record.dateTime}"),
+            subtitle: Text("Peso: ${record.weight}, Altura: ${record.height}"),
+          );
+        },
+      ),
+    );
+  }
+}
+
